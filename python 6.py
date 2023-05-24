98ab244fb589a65cd44745182e8379469ed6c9cd
siddunandish <134358917+siddunandish@users.noreply.github.com>
2023-05-24 10:54:07 +0530

    Add files via upload

---
+++ python 6.py
@@ -0,0 +1,10 @@
+#write the program to find the biggest of two numbers using functions#
+
+def big(a,b):
+ if(a>b):
+    print('a is bigger')
+ else:
+    print('b is bigger')
+c=int(input("enter first value"))
+d=int(input("enter second value"))
+big(c,d)
